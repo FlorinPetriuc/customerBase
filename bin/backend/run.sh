17a28fc3dd661e0f78c0689d881654fc45420364
Florin <pfl@northq.com>
2017-12-23 09:29:06 +0100

    added run.sh parameters

--- bin/backend/run.sh
+++ bin/backend/run.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-java -classpath bin:lib/* customerBase.CustomerBase
+java -classpath bin:lib/* customerBase.CustomerBase -dbuser=propuser -dbpass=propuser -dburl=localhost -db=propellerhead -port=4080
